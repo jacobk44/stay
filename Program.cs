2537a07e7e5e061ebee8d60b04891d530651fb32
jacobkwartengamoah@gmail.com <>
2025-07-03 01:16:06 +0000

    new code added

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Whqt is your name? ");
+string name = Console.ReadLine();
+Console.WriteLine(name);
